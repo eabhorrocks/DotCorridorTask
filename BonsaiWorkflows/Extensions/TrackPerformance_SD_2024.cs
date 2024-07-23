d04d5222ceb96f7e84b306e46839551bee9509ae
eabhorrocks <36156626+eabhorrocks@users.noreply.github.com>
2024-07-23 17:44:36 +0100

    performance tracking for speed discrimination task

--- BonsaiWorkflows/Extensions/TrackPerformance_SD_2024.cs
+++ BonsaiWorkflows/Extensions/TrackPerformance_SD_2024.cs
@@ -10,46 +10,58 @@ using System.Reactive.Linq;
 [WorkflowElementCategory(ElementCategory.Transform)]
 public class TrackPerformance_SD_2024
 {
-    public List<int> TrialCountsArray {get; set; }
-    public List<int> TrialCorrectArray {get; set; }
-    public List<int> RightResponseArray {get; set; }
+    public List<int> TrialCountsList {get; set; }
+    public List<int> TrialCorrectList {get; set; }
+    public List<int> RightResponseList {get; set; }
+    public List<int> TrialEngagedList {get; set; }
 
-    public IObservable<Tuple<List<int>, List<int>, List<double>>> Process(IObservable<Tuple<float, int, int>> source)
+    public IObservable<Tuple<List<int>, List<int>, List<double>, List<double>>> Process(IObservable<Tuple<float, int, int>> source)
     {
         return source.Select(value => 
         {
-            int nSpeedPairs = TrialCountsArray.Count();
+            int nSpeedPairs = TrialCountsList.Count();
             float response = value.Item1;
             int trialResult = value.Item2;
             int speedPairIndex = value.Item3;
-            List<double> pCorrectArray = new List<double>();
+            List<double> pCorrectList = new List<double>();
+            List<double> pEngagedList = new List<double>();
+
             Console.WriteLine("index: " + speedPairIndex);
             
 
-            TrialCountsArray[speedPairIndex] = TrialCountsArray[speedPairIndex]+1;
+            TrialCountsList[speedPairIndex] = TrialCountsList[speedPairIndex]+1;
             //Console.WriteLine(TrialCountsArray[speedPairIndex]);
 
             if (trialResult==1 | trialResult==-1)
             {
-                TrialCorrectArray[speedPairIndex]=TrialCorrectArray[speedPairIndex]+1;
-                Console.WriteLine(TrialCorrectArray[speedPairIndex]);
+                TrialCorrectList[speedPairIndex]=TrialCorrectList[speedPairIndex]+1;
+                Console.WriteLine(TrialCorrectList[speedPairIndex]);
+            }
+
+            if (trialResult!=3)
+            {
+                TrialEngagedList[speedPairIndex]=TrialEngagedList[speedPairIndex]+1;
             }
 
             
             Console.WriteLine("pcorr");
             for (int i=0; i<nSpeedPairs; i++)
             {
-                double nCorrect = (double)TrialCorrectArray[i];
-                double nCount = (double)TrialCountsArray[i];
+                double nCorrect = (double)TrialCorrectList[i];
+                double nCount = (double)TrialCountsList[i];
+                double nEngaged = (double)TrialEngagedList[i];
                 double pCorrect = nCorrect/nCount;
-                pCorrectArray.Add(pCorrect);
+                double pEngaged = nEngaged/nCount;
+
+                pCorrectList.Add(pCorrect);
+                pEngagedList.Add(pEngaged);
                 Console.WriteLine("ncorr: " + nCorrect + " nCount: " + nCount + " pCorr" + pCorrect);
 
             }
 
             
 
-            var output = new Tuple<List<int>, List<int>, List<double>>(TrialCountsArray, TrialCorrectArray, pCorrectArray);
+            var output = new Tuple<List<int>, List<int>, List<double>, List<double>>(TrialCountsList, TrialCorrectList, pCorrectList, pEngagedList);
 
             return output;
 
